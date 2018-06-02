872f237449a68d1c4383206fd15be09567bcd20b
Gaurav Rawal <gaurav.rawal66@gmail.com>
2018-06-02 20:55:45 +0530

    services and changes in the ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AskComponent } from './ask/ask.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   declarations: [
@@ -20,9 +26,11 @@ import { LeaderboardComponent } from './leaderboard/leaderboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ValidateService, AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-questions',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor() { }
+  public valueSelected = 'date';
+  public datasource: DataSource[];
+
+  public sortValues: SortValues[] = [
+    {'id': 1, 'name': 'recent', 'value': 'date'},
+    {'id': 2, 'name': 'views', 'value': 'views'}
+  ];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  onSelect(sortValue) {
+    this.valueSelected = sortValue;
+  }
+
+  getSortedData( sortBy) {
+    this.http.post<SortedDataStructure>('http://localhost:3000/questions/sort', {
+      'field': this.valueSelected
+    }
+  ).subscribe(data => {
+      this.datasource = data.msg;
+      console.log(data.msg);
+      console.log('datasource');
+      console.log(this.datasource);
+    });
+  }
+}
+
+class SortValues {
+  id: number;
+  name: String;
+  value: String;
+}
+
+class DataSource {
+  question: String;
+  createdOn: Date;
+  username: String;
+  views: Number;
+  answers: Object[];
+}
+
+class SortedDataStructure {
+  success: boolean;
+  msg: DataSource[];
 }
